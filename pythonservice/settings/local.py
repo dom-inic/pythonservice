267c941fc5565ab0e78150acc597ca59c968be78
dom-inic <dominicnyambane22@gmail.com>
2024-02-19 16:43:15 +0300

    redis

--- pythonservice/settings/local.py
+++ pythonservice/settings/local.py
@@ -10,9 +10,11 @@ DATABASES = {
     }
 }
 
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PORT = os.environ['REDIS_PORT']
-REDIS_DB = os.environ['REDIS_DB']
+
+REDIS_HOST = 'localhost'
+REDIS_HOST = 'redis'
+REDIS_PORT = '6379'
+REDIS_DB = 1
 
 # Celery Configuration docker
 # CELERY_BROKER_URL = os.environ['CELERY_RESULT_BACKEND']
